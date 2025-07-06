850cc19d872ce79ae6f5c65b9e8077baabfda312
Akito Kimura <aparatia.79ez.evol@gmail.com>
2025-07-06 11:24:05 +0900

    第２０回

--- StudentManagement/src/main/java/studentmanagement/StudentManagement/StudentManagementApplication.java
+++ StudentManagement/src/main/java/studentmanagement/StudentManagement/StudentManagementApplication.java
@@ -6,32 +6,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import studentmanagement.StudentManagement.data.Student;
+import studentmanagement.StudentManagement.data.Students_Courses;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	@Autowired
-	private StudentRepository repository;
-
-	@Autowired
-	private Students_CoursesRepository coursesRepository;
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/studentList")
-
-	public List<Student> getStudentList() {
-		return repository.search();
-	}
-
-	@GetMapping("/students_coursesList")
-
-	public List<Students_Courses> getStudents_CoursesList(){
-		return coursesRepository.search();
-	}
+//	@GetMapping("/studentList")
+//
+//	public List<Student> getStudentList() {
+//		return repository.search();
+//	}
+//
+//	@GetMapping("/students_coursesList")
+//
+//	public List<Students_Courses> getStudents_CoursesList(){
+//	//	return coursesRepository.search();
+//		return repository.searchStudentCourses();
+//	}
 }
-//test
+//
--- StudentManagement/src/main/java/studentmanagement/StudentManagement/StudentRepository.java
+++ StudentManagement/src/main/java/studentmanagement/StudentManagement/StudentRepository.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import studentmanagement.StudentManagement.data.Student;
+import studentmanagement.StudentManagement.data.Students_Courses;
 
+/*
+受講生情報を取り扱うリポジトリ
+
+全体検索や単一条件で検索　生徒情報の検索が行える
+ */
 
 @Mapper
 public interface StudentRepository {
@@ -12,4 +19,9 @@ public interface StudentRepository {
   @Select("SELECT * FROM students")
   List<Student> search();
 
+  @Select("SELECT * FROM students_courses")
+  List<Students_Courses> searchStudentCourses();
+
+
 }
+
